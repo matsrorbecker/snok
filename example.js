92b00747534c12be16a5e3a302b8f610e83e8d38
Mats Rörbecker <mats@rorbecker.com>
2016-06-28 10:27:42 +0200

    Adds example script to repo

---
+++ example.js
@@ -0,0 +1,23 @@
+const Snok = require('snok');
+
+const feeds = [
+	'https://news.ycombinator.com/rss'
+]
+
+const triggers = [
+	'machine learning',
+	'artificial intelligence',
+	'automation'
+]
+
+const notify = function (item) {
+	console.log("Hey, there seems to be an interesting article out there!");
+	console.log(item.link);
+}
+
+const secondsBetweenChecks = 60;
+
+const snok = new Snok(feeds, triggers, notify);
+
+setInterval(snok.sniff, secondsBetweenChecks * 1000);
+snok.sniff();
